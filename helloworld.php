b32c5825d27b7c3c8e7d12096a1f204177a6533e
dmikab <dmikab@greeny.cs.tlu.ee>
2016-02-11 12:55:35 +0200

    added statement

---
+++ helloworld.php
@@ -0,0 +1,33 @@
+ <?php
+ 
+ // Variable starts with $,
+ //string has to quotation marks
+ // line ends with semicolon
+ 
+   $first_name = "Dmitri";
+    $last_name = "Kablutško";
+   
+   //show code to browser
+   // for adding up numbers + signs
+   // for adding up strings .
+   echo $first_name." ".$last_name; 
+  // > greater than
+  //< smaller than
+  //>= greater than or equal
+  //<=smaller than or equal
+  //== equal
+  //!= not equal
+  
+  $age = 5;
+  
+  //if age is smaller than or equal to 6, then y
+ if($age <=5){
+	 
+	 //it is true
+	 echo "yes you can sleep at lunchtime";
+	 }else{
+	 //it is false
+	 echo " no, you are at school";
+ }
+ 
+ ?>
